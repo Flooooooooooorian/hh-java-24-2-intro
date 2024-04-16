f0ee16fc4e158ebe57f885803d036935460a4031
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-04-16 17:23:01 +0200

    Add Random stuff

--- IfMethod.java
+++ IfMethod.java
@@ -3,16 +3,16 @@ public class IfMethod {
     public static void main(String[] args) {
         int age = 99;
 
-        String result = checkIfPersonIs18(478);
+        String result = checkIfPersonIs18( 478);
 
         System.out.println(result);
     }
 
     public static String checkIfPersonIs18(int age) {
-        System.out.println("Methode checkIfPersonIs18");
 
         if (age > 18) {
-            System.out.println("Ja Person ist älter als 18!");
+            System.out.println("Ja Person ist älter als 18!");        System.out.println("Methode checkIfPersonIs18");
+
             return "18";
         } else if (age < 18) {
             System.out.println("Nein, Person ist jünger als 18.");
@@ -21,6 +21,14 @@ public class IfMethod {
             System.out.println("Ist genau 18!");
             return "18";
         }
+    }
+
+    public static String checkIfPersonIs18(int age, String name) {
+        return "";
+    }
+
+    public static String checkIfPersonIs18(String name, int age) {
+        return "";
     }
 
     public static void testAbc(String name) {
---
+++ out/production/hh-java-24-2-test/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,6 @@
+
+8
+test.txt,4/b/4b6fcb2d521ef0fd442a5301e7932d16cc9f375a
+:
+
+Intro.java,6/9/69c32169a64c36a68e009c2a50f64a346a0d7a16
