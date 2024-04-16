2c91623e550d9a6097548968cfdfc715e5c7c487
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-04-16 10:40:34 +0200

    Add Java Basics

---
+++ Intro.java
@@ -0,0 +1,62 @@
+public class Intro {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hallo Welt!!!");
+        System.out.println("TEST");
+        System.out.println("hh-java-24-2");
+
+        short myShort = 18;
+        System.out.println(myShort);
+
+        int myNumber = 20;
+
+        System.out.println(myNumber);
+
+        long myLong = 19;
+        System.out.println(myLong);
+
+        myNumber = 21;
+
+        System.out.println(myNumber);
+
+        double myDouble = 20.5;
+        System.out.println(myDouble);
+
+        float myFloat = 2.5F;
+        System.out.println(myFloat);
+
+        String myString = "Hallo";
+
+        System.out.println(myString);
+
+        char myChar = 'a';
+
+        System.out.println(myChar);
+
+        boolean myBoolean = true; // false
+
+        System.out.println(myBoolean);
+
+        myFloat = 3.875F;
+
+        System.out.println(myFloat);
+
+
+        System.out.println(myNumber / 999);
+        System.out.println("Hallo" + " Welt");
+        System.out.println(myDouble * 3.8);
+
+        System.out.println(7 + 8.5);
+
+        System.out.println(5 == 8);
+        System.out.println(5 == 5);
+        System.out.println(894 >= 5);
+
+        System.out.println("HALLO" == "HALLO");
+
+        System.out.println("myDobule:");
+        System.out.println(myDouble == 19.5 || myDouble == 18.5 || myDouble == 20.5);
+        System.out.println("myNumber: " + (myNumber > 0 && myNumber < 50));
+    }
+}
